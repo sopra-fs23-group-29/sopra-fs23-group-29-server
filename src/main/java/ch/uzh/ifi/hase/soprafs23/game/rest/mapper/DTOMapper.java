8e87b455da46803146b93d6ce475155c78c3ff66
mwooll <mark.woolley@uzh.ch>
2023-04-04 15:46:15 +0200

    Deleting a User actually works now

--- src/main/java/ch/uzh/ifi/hase/soprafs23/game/rest/mapper/DTOMapper.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/game/rest/mapper/DTOMapper.java
@@ -4,7 +4,6 @@ import ch.uzh.ifi.hase.soprafs23.game.entity.User;
 import ch.uzh.ifi.hase.soprafs23.game.rest.dto.UserGetDTO;
 import ch.uzh.ifi.hase.soprafs23.game.rest.dto.UserPostDTO;
 import ch.uzh.ifi.hase.soprafs23.game.rest.dto.UserPutDTO;
-import ch.uzh.ifi.hase.soprafs23.game.rest.dto.UserDeleteDTO;
 import org.mapstruct.*;
 import org.mapstruct.factory.Mappers;
 
@@ -22,26 +21,22 @@ import org.mapstruct.factory.Mappers;
 @Mapper
 public interface DTOMapper {
 
-  DTOMapper INSTANCE = Mappers.getMapper(DTOMapper.class);
+    DTOMapper INSTANCE = Mappers.getMapper(DTOMapper.class);
 
-  @Mapping(source = "username", target = "username")
-  @Mapping(source = "password", target = "password")
-  @Mapping(source = "token", target = "token")
-  User convertUserPostDTOtoEntity(UserPostDTO userPostDTO);
+    @Mapping(source = "username", target = "username")
+    @Mapping(source = "password", target = "password")
+    @Mapping(source = "token", target = "token")
+    User convertUserPostDTOtoEntity(UserPostDTO userPostDTO);
 
-  @Mapping(source = "id", target = "id")
-  @Mapping(source = "username", target = "username")
-  @Mapping(source = "status", target = "status")
-  @Mapping(source = "creationDate", target = "creationDate")
-  @Mapping(source = "birthday", target = "birthday")
-  UserGetDTO convertEntityToUserGetDTO(User user);
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "username", target = "username")
+    @Mapping(source = "status", target = "status")
+    @Mapping(source = "creationDate", target = "creationDate")
+    @Mapping(source = "birthday", target = "birthday")
+    UserGetDTO convertEntityToUserGetDTO(User user);
 
-  @Mapping(source = "username", target = "username")
-  @Mapping(source = "birthday", target = "birthday")
-  @Mapping(source = "password", target = "password")
-  User convertUserPutDTOtoEntity(UserPutDTO userPutDTO);
-
-  @Mapping(source = "username", target = "username")
-  @Mapping(source = "password", target = "password")
-  User convertUserDeleteDTOtoEntity(UserDeleteDTO userDeleteDTO);
+    @Mapping(source = "username", target = "username")
+    @Mapping(source = "birthday", target = "birthday")
+    @Mapping(source = "password", target = "password")
+    User convertUserPutDTOtoEntity(UserPutDTO userPutDTO);
 }
--- src/test/java/ch/uzh/ifi/hase/soprafs23/game/rest/mapper/DTOMapperTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/game/rest/mapper/DTOMapperTest.java
@@ -5,7 +5,6 @@ import ch.uzh.ifi.hase.soprafs23.game.entity.User;
 import ch.uzh.ifi.hase.soprafs23.game.rest.dto.UserGetDTO;
 import ch.uzh.ifi.hase.soprafs23.game.rest.dto.UserPostDTO;
 import ch.uzh.ifi.hase.soprafs23.game.rest.dto.UserPutDTO;
-import ch.uzh.ifi.hase.soprafs23.game.rest.dto.UserDeleteDTO;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -16,68 +15,53 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * works.
  */
 public class DTOMapperTest {
-  @Test
-  public void testCreateUser_fromUserPostDTO_toUser_success() {
-    // create UserPostDTO
-    UserPostDTO userPostDTO = new UserPostDTO();
-    userPostDTO.setPassword("password");
-    userPostDTO.setUsername("username");
+    @Test
+    public void testCreateUser_fromUserPostDTO_toUser_success() {
+        // create UserPostDTO
+        UserPostDTO userPostDTO = new UserPostDTO();
+        userPostDTO.setPassword("password");
+        userPostDTO.setUsername("username");
 
-    // MAP -> Create user
-    User user = DTOMapper.INSTANCE.convertUserPostDTOtoEntity(userPostDTO);
+        // MAP -> Create user
+        User user = DTOMapper.INSTANCE.convertUserPostDTOtoEntity(userPostDTO);
 
-    // check content
-    assertEquals(userPostDTO.getPassword(), user.getPassword());
-    assertEquals(userPostDTO.getUsername(), user.getUsername());
-  }
+        // check content
+        assertEquals(userPostDTO.getPassword(), user.getPassword());
+        assertEquals(userPostDTO.getUsername(), user.getUsername());
+    }
 
-  @Test
-  public void testGetUser_fromUser_toUserGetDTO_success() {
-    // create User
-    User user = new User();
-    user.setPassword("Password");
-    user.setUsername("firstname@lastname");
-    user.setStatus(UserStatus.OFFLINE);
-    user.setToken("1");
+    @Test
+    public void testGetUser_fromUser_toUserGetDTO_success() {
+        // create User
+        User user = new User();
+        user.setPassword("Password");
+        user.setUsername("firstname@lastname");
+        user.setStatus(UserStatus.OFFLINE);
+        user.setToken("1");
 
-    // MAP -> Create UserGetDTO
-    UserGetDTO userGetDTO = DTOMapper.INSTANCE.convertEntityToUserGetDTO(user);
+        // MAP -> Create UserGetDTO
+        UserGetDTO userGetDTO = DTOMapper.INSTANCE.convertEntityToUserGetDTO(user);
 
-    // check content
-    assertEquals(user.getId(), userGetDTO.getId());
-    assertEquals(user.getUsername(), userGetDTO.getUsername());
-    assertEquals(user.getStatus(), userGetDTO.getStatus());
-  }
+        // check content
+        assertEquals(user.getId(), userGetDTO.getId());
+        assertEquals(user.getUsername(), userGetDTO.getUsername());
+        assertEquals(user.getStatus(), userGetDTO.getStatus());
+    }
 
-  @Test
-  public void testUpdateUser_success() {
+    @Test
+    public void testUpdateUser_success() {
 
-    // create put DTO
-    UserPutDTO userPutDTO = new UserPutDTO();
-    userPutDTO.setBirthday("1990-01-01");
-    userPutDTO.setUsername("testUsername");
+        // create put DTO
+        UserPutDTO userPutDTO = new UserPutDTO();
+        userPutDTO.setBirthday("1990-01-01");
+        userPutDTO.setUsername("testUsername");
 
-    // MAP to User object
-    User user = DTOMapper.INSTANCE.convertUserPutDTOtoEntity(userPutDTO);
+        // MAP to User object
+        User user = DTOMapper.INSTANCE.convertUserPutDTOtoEntity(userPutDTO);
 
-    // check content
-    assertEquals(userPutDTO.getBirthday(), user.getBirthday());
-    assertEquals(userPutDTO.getUsername(), user.getUsername());
+        // check content
+        assertEquals(userPutDTO.getBirthday(), user.getBirthday());
+        assertEquals(userPutDTO.getUsername(), user.getUsername());
 
-  }
-
-  @Test
-  public void testCreateUser_fromUserDeleteDTO_toUser_success() {
-    // create UserPostDTO
-    UserDeleteDTO userDeleteDTO = new UserDeleteDTO();
-    userDeleteDTO.setUsername("username");
-    userDeleteDTO.setPassword("password");
-
-    // MAP -> Create user
-    User user = DTOMapper.INSTANCE.convertUserDeleteDTOtoEntity(userDeleteDTO);
-
-    // check content
-    assertEquals(userDeleteDTO.getUsername(), user.getUsername());
-    assertEquals(userDeleteDTO.getPassword(), user.getPassword());
-  }
+    }
 }
