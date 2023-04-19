d8967e2f5c857cc06d1aa2c564f49c760fc94257
Darnol <32634736+Darnol@users.noreply.github.com>
2023-04-19 08:32:00 +0200

    BarrierQuestion now generates 5 options, always sends them all to the client
    Add test tempCountry == null from CountryService if ciocCode is faulty

--- src/main/java/ch/uzh/ifi/hase/soprafs23/game/questions/restCountry/BarrierQuestion.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/game/questions/restCountry/BarrierQuestion.java
@@ -4,20 +4,27 @@ import ch.uzh.ifi.hase.soprafs23.constant.BarrierQuestionEnum;
 import ch.uzh.ifi.hase.soprafs23.constant.QuestionType;
 import ch.uzh.ifi.hase.soprafs23.game.entity.Country;
 
+import java.util.List;
+
 public class BarrierQuestion {
+
+    public static final int NOPTIONS = 5; // always give 5 DIFFERENT options, one of the the correct one
 
     private final QuestionType questionType;
     private final BarrierQuestionEnum barrierQuestionEnum;
     private final String questionText;
     private final Country country;
+    private List<Integer> answerOptions;
+    private List<Country> countryOptions;
     private int correctResult;
 
-    public BarrierQuestion(BarrierQuestionEnum barrierQuestionEnum, Country country) {
+    public BarrierQuestion(BarrierQuestionEnum barrierQuestionEnum, Country country, List<Country> countryOptions) {
         this.questionType = QuestionType.BARRIER;
         this.country = country;
+        this.countryOptions = countryOptions;
         this.barrierQuestionEnum = barrierQuestionEnum;
         this.questionText = this.barrierQuestionEnum.getQuestion();
-        setCorrectResult();
+        setCorrectResultAndOptions();
     }
 
     // The Guessed Rank is given like 1,2,3... with 1 being the largest value, the country should be given in the cioc code
@@ -33,12 +40,11 @@ public class BarrierQuestion {
         return country;
     }
 
-    private void setCorrectResult() {
+    private void setCorrectResultAndOptions() {
         switch (this.barrierQuestionEnum) {
-            case NBORDERS -> correctResult = country.getNBorders();
+            case NBORDERS:
+                correctResult = country.getNBorders();
+                answerOptions = countryOptions.stream().map(Country::getNBorders).toList();
         }
     }
-
-
-
 }
