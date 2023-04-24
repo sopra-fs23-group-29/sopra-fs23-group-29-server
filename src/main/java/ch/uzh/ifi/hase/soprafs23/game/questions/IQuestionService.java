2e1d84b8e6c9066cb239bbbe414bc4e155e3d029
Darnol <32634736+Darnol@users.noreply.github.com>
2023-04-24 14:06:36 +0200

    Fetch 5 or 6 countries in ranking question, depending on number of players
    Remove old todo

--- src/main/java/ch/uzh/ifi/hase/soprafs23/game/questions/IQuestionService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/game/questions/IQuestionService.java
@@ -6,8 +6,6 @@ import ch.uzh.ifi.hase.soprafs23.game.questions.restCountry.RankingQuestion;
 public interface IQuestionService {
 
   RankingQuestion generateRankQuestion(int size);
-
-  // todo: Implement barrierQuestion
   BarrierQuestion generateBarrierQuestion();
 
 }
