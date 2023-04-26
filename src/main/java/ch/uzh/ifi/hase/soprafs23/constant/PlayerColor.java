666403ea8246df55d2fe8abb97498a34441dbb00
moonaaaw <116147381+moonaaaw@users.noreply.github.com>
2023-04-26 12:46:11 +0200

    updated player colors

--- src/main/java/ch/uzh/ifi/hase/soprafs23/constant/PlayerColor.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/constant/PlayerColor.java
@@ -1,5 +1,5 @@
 package ch.uzh.ifi.hase.soprafs23.constant;
 
 public enum PlayerColor {
-  NOTSET, RED, BLUE, GREEN, YELLOW, MAGENTA, CYAN
+  NOTSET, INDIANRED, ORANGE, YELLOW, LIGHTGREEN, LIGHTSKYBLUE, VIOLET
 }
