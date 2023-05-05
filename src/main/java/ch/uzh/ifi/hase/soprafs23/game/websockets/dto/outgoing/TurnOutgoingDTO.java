a1d0aa212a79aeff3315f23e00d27aa85a540d62
Darnol <32634736+Darnol@users.noreply.github.com>
2023-05-05 16:01:46 +0200

    Remove unneeded getters and setters

--- src/main/java/ch/uzh/ifi/hase/soprafs23/game/websockets/dto/outgoing/TurnOutgoingDTO.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/game/websockets/dto/outgoing/TurnOutgoingDTO.java
@@ -23,26 +23,8 @@ public class TurnOutgoingDTO {
     this.takenGuesses = turn.getTakenGuesses();
   }
 
-  public int getTurnNumber() {
-    return turnNumber;
-  }
-  public void setTurnNumber(int turnNumber) {
-    this.turnNumber = turnNumber;
-  }
-  public List<Player> getTurnPlayers() {
-    return turnPlayers;
-  }
-  public void setTurnPlayers(List<Player> turnPlayers) {this.turnPlayers = turnPlayers;}
-  public RankingQuestion getRankQuestion() {
-    return rankQuestion;
-  }
-  public void setRankQuestion(RankingQuestion rankQuestion) {
-    this.rankQuestion = rankQuestion;
-  }
   public List<Guess> getTakenGuesses() {
     return takenGuesses;
   }
-  public void setTakenGuesses(List<Guess> takenGuesses) {
-    this.takenGuesses = takenGuesses;
-  }
+
 }
